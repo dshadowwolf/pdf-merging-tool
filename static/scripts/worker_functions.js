6c4db4586e5eda2c355d180a23dbe99dd5217df6
Daniel Hazelton <dshadowwolf@gmail.com>
2020-10-10 17:56:43 -0400

    the 'make it pretty' phase is basically done - just some code to hook up the toolbar and make the preview of the final result possible

--- static/scripts/worker_functions.js
+++ static/scripts/worker_functions.js
@@ -5,40 +5,52 @@ ipcRenderer.on(`worker-show-data`, (e, data) => {
 	displayData(data);
 });
 
-function makeCell(contents) {
-	var cell = $('<td></td>');
+function makeCell(contents, classes) {
+	let classlist = "no-cell-borders";
+	if (classes)
+		classes.forEach( classname => classlist = `${classlist} ${classname}` );
+	
+	let cell = $(`<td class="${classlist}"></td>`);
 	cell.append(contents);
 	return cell;
 }
 
+function makeSize(filesize, mark) {
+	let marks = [ 'Bytes', 'KiB', 'MiB', 'GiB', 'TiB', '???' ];
+	
+	if (filesize > 1024) return makeSize(filesize/1024, ++mark);
+	else {
+		let sz = filesize.toFixed(2).toString().padStart(6, " ");
+		let sp = marks[mark > marks.length?5:mark].padEnd(5, " ");
+		return `${sz} ${sp}`;
+	}
+}
+
 function makeRow(filedata) {
-	var row = $('<tr class="list-item"></tr>');
-	var button = $('<button type=button class="view-file-btn"><image class="view-button" src="images/magnifying-search-lenses-tool.svg"/></button>');
-	button.on('click', (event) => { window.viewFile(path.resolve(filedata.path)); event.preventDefault(); });
-	var button_row = $('<td class="view-button-cell"></td>');
-	button_row.append(button);
-	row.append(button_row);
-	row.append(makeCell(filedata.path));
-	row.append(makeCell(filedata.type));
-	row.append(makeCell(`${filedata.size} bytes`));
+	let row         = $('<tr class="list-item"></tr>');
+	let btn         = $(`<button type=button class="view-file-btn tooltip"><span class="tooltiptext">View ${path.basename(filedata.path)}</span><image class="view-button" src="images/search.svg"/></button>`);
+	let size_cell   = makeCell(makeSize(filedata.size, 0));
+	
+	btn.on('click', (event) => { window.viewFile(path.resolve(filedata.path)); event.preventDefault(); });
+	
+	row.append(makeCell(btn, [ 'view-button-cell' ]));
+	row.append(makeCell(filedata.path, [ 'name-cell' ]));
+	row.append(makeCell(makeSize(filedata.size, 0), [ 'size-cell' ]));
+	
 	return row;
 }
 
 function makeTableData(file_list) {
 	var rv = [];
 	
-	for ( file of file_list ) rv.push(makeRow(file));
+	file_list.filter( (file) => file.type === 'application/pdf' ).forEach( (file) => rv.push(makeRow(file)) );
 	
 	return rv;
 }
 
 function displayData(data) {
-	let t = "";
+	let table_body = $('#data_display > table.file-list > tbody');
+	table_body.text('');
 
-	let q = $('#data_display > table.file-list > tbody');
-	q.innerText = '';
-	
-	for ( row of makeTableData(data) ) {
-		q.append(row);
-	}
+	makeTableData(data).forEach( row => table_body.append(row) )
 }
