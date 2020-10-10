d7df99b56f05afaf50b6a176ffdd34676d4dbeeb
Daniel Hazelton <dshadowwolf@gmail.com>
2020-10-09 23:36:18 -0400

    fix up the viewer to be wired in properly and split some stuff off better for more localized CSS

--- static/scripts/drag_drop.js
+++ static/scripts/drag_drop.js
@@ -15,14 +15,7 @@ document.addEventListener('drop', (event) => {
     event.preventDefault(); 
     event.stopPropagation(); 
 
-    var data = [];
-
-    for (const f of event.dataTransfer.files) { 
-        // Using the path attribute to get absolute file path 
-        data.push( { name: f.path, extension: getExtension(f.path) } );
-    }
-    console.log(window.inspectData(data));
-    document.body.dispatchEvent(new CustomEvent('dataReady', { detail: data, bubbles: true, cancelable: false }));
+	window.showWorker(event.dataTransfer.files);
 });
 
 document.addEventListener('dragover', (e) => { 
@@ -31,36 +24,11 @@ document.addEventListener('dragover', (e) => {
 }); 
 
 document.addEventListener('dragenter', (event) => { 
-    console.log('File is in the Drop Space'); 
+ //   console.log('File is in the Drop Space'); 
 }); 
 
 document.addEventListener('dragleave', (event) => { 
-    console.log('File has left the Drop Space'); 
+//    console.log('File has left the Drop Space'); 
 }); 
-
-document.addEventListener('dataReady', (event) => {
-    let fdata = event.detail;
-    let doc = document.getElementById('data_display');
-    doc.innerHtml = "";
-    doc.innerText = "";
-    let filtered = fdata.filter((item) => item.extension === 'pdf');
-    let wrapper = document.createElement('ul');
-    wrapper.className = 'file-list';
-    for ( const item of filtered ) {
-        let it = document.createElement('li');
-        it.className = 'file-list-item';
-        let txt = document.createTextNode(basename(item.name));
-        it.appendChild(txt);
-        //let name = encodeURIComponent(item.name);
-
-        it.addEventListener('click',  (event) => {
-            let p = window.path.resolve(window.dirpath, `../viewer.html`);
-            window.open(encodeURI(`file://${p}?file=${item.name}`), '_blank', "preload=./scripts/preload.js, frame=yes, toolbar=no, menubar=no");
-        });
-        wrapper.appendChild(it);
-    }
-    window.sendLoadedFileList(filtered);
-    doc.appendChild(wrapper);
-});
 
 
--- static/scripts/viewer_functions.js
+++ static/scripts/viewer_functions.js
@@ -1,9 +1,14 @@
-$(document).ready( () => displayDefault("../../other/blank.pdf") );
+const { ipcRenderer } = require('electron');
+const path = require('path');
 
-function displayDefault(pagename) {
+$(document).ready( () => display(path.resolve(__dirname, "other/blank.pdf")) );
+
+function display(pagename) {
 	const elem = document.getElementById('data_display');
 	const disp = document.createElement('iframe');
 	elem.innerHTML = "";
 	disp.src = `./pdfjs/web/viewer.html?file=${pagename}`
 	elem.appendChild(disp);
 }
+
+ipcRenderer.on('viewer-show-file', (e, data) => display(data));
